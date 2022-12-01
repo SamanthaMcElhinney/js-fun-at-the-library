d8fd15662c8cda77dd3fc90e8b2b006e29288191
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2022-11-30 20:35:19 -0800

    Add changes to book

--- src/book.js
+++ src/book.js
@@ -1,7 +1,29 @@
 function createTitle() {
-  
+  var bookIdea = createTitle.add(the)
+  return bookIdea 
+  }
+
+function buildMainCharacter() {
+
 }
 
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20
+}
+
+function writeBook() {
+  var book = {
+    title:
+    mainCharacter:
+    pageCount:
+    genre:
+  }
+  return book
+}
+
+function editBook() {
+
+}
 module.exports = {
   createTitle,
   // buildMainCharacter,
