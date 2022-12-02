9924af36dca0bc32f6e35157006998cbe70266a1
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2022-12-01 20:09:51 -0800

    Add changes

--- src/book.js
+++ src/book.js
@@ -1,34 +1,46 @@
-function createTitle() {
-  var bookIdea = createTitle.add(the)
-  return bookIdea 
+function createTitle(newTitle) {
+  return (`The ${newTitle}` )
   }
 
-function buildMainCharacter() {
-
-}
-
-function calculatePageCount(bookTitle) {
-  return bookTitle.length * 20
-}
-
-function writeBook() {
-  var book = {
-    title:
-    mainCharacter:
-    pageCount:
-    genre:
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter1 = {
+    name: name,
+    age: age,
+    pronouns: pronouns
   }
+  return mainCharacter1
+}
+
+function saveReview (review, array) {
+  array.push(review)
+    if (array.indexOf(review)) {
+      return review
+    }
+}
+
+function calculatePageCount(title) {
+  return title.length * 20
+}
+
+function writeBook(title, mainCharacter,genre) {
+  var book : {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount (title),
+    genre: genre
+  }
+
   return book
-}
-
+  
 function editBook() {
 
 }
+
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  //editBook
 }
