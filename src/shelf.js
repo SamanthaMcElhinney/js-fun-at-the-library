d1c2eda98b880323f4caf3cd0cce7babe8e07a6b
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2022-12-03 15:21:17 -0800

    Add changes

--- src/shelf.js
+++ src/shelf.js
@@ -1,35 +1,37 @@
 function shelfBook(books, shelf) {
   if (shelf.length < 3) {
-  return shelf.unshift(books);
+    return shelf.unshift(books);
   }
 }
 
 function unshelfBook(book, shelf) {
-    for (var i = 0; i < shelf.length; i++) {
-      if (shelf[i].title === book){
-      return shelf.splice(i,1)
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === book) {
+      return shelf.splice(i, 1);
     }
   }
 }
 
 function listTitles(shelf) {
-  var titles = []
-  for (var i = 0; i <shelf.length; i++) {
-    titles.push(shelf[i].title)
-  } return titles.join (', ')
+  var titles = [];
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+  }
+  return titles.join(", ");
 }
 
 function searchShelf(shelf, book) {
   for (var i = 0; i < shelf.length; i++) {
-    if(shelf[i].title.includes(book))
+    if (shelf[i].title.includes(book)) {
+      return true;
+    }
   }
-  return shelf[i].titles
+  return false;
 }
-  
 
 module.exports = {
-   shelfBook, 
-   unshelfBook,
-   listTitles,
-   searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf,
 };
